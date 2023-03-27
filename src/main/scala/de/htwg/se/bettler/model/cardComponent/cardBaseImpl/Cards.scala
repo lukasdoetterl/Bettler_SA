b71603c08ad8e554972f804bebc9aa23f4e33fc0
lukasdoetterl <lukasdoetterl@hotmail.com>
2023-03-27 15:37:42 +0200

    Foreach umgewandelt in map

--- src/main/scala/de/htwg/se/bettler/model/cardComponent/cardBaseImpl/Cards.scala
+++ src/main/scala/de/htwg/se/bettler/model/cardComponent/cardBaseImpl/Cards.scala
@@ -5,10 +5,12 @@ package cardBaseImpl
 
 import model.cardComponent.cardBaseImpl.Card
 
+import scala.language.postfixOps
+
 case class Cards(cards : Set[CardInterface]) extends CardsInterface:
     override def toString =
         var string = ""
-        cards.foreach(c => string += c.toString)
+        cards.map(c => string += c.toString)
         string
 
     def returnSet = cards
@@ -24,20 +26,22 @@ case class Cards(cards : Set[CardInterface]) extends CardsInterface:
         if c.returnSet.isEmpty then return false
         if cards.size == 0 then return true
         if c.returnSet.size != cards.size then return false
-        for {
-            c1 <- cards
-            c2 <- c.returnSet
-            if !c2.isHigher(c1)
-        } return false
+        cards
+          .map(c1 =>
+              c.returnSet
+                .withFilter(c2 => !c2.isHigher(c1))
+                .foreach(c2 => return false)
+          )
         return true
 
     def isPlayable : Boolean =
         if cards.isEmpty then return false
-        for {
-            c1 <- cards
-            c2 <- cards
-            if !c1.sameValue(c2)
-        } return false
+        cards
+          .map(c1 =>
+              cards
+                .withFilter(c2 => !c1.sameValue(c2))
+                .foreach(c2 => return false)
+          )
         return true
 
     def size = cards.size
@@ -46,22 +50,24 @@ case class Cards(cards : Set[CardInterface]) extends CardsInterface:
         var group = Vector.empty[Cards]
         for (value <- 7 to 14)
             var groupCards = Set.empty[CardInterface]
-            for (card <- cards)
-                if card.intValue.equals(value) then
-                    groupCards += card
+            cards
+              .map(card => if card.intValue.equals(value) then
+                  groupCards += card)
             if groupCards.nonEmpty then
                 group = group :+ Cards(groupCards)
         return group
 
 
     def findPlayable(board : CardsInterface) : Option[CardsInterface] =
-        for (c <- this.groupBySameValue)
-            if board.isWorse(c) then return Some(c)
-        for (c <- this.groupBySameValue)
-            if c.size > board.size then
-                var reducedCards = Set.empty[CardInterface]
-                c.returnSet.toArray.slice(0, board.size).foreach{x => reducedCards = reducedCards + x}
-                if board.isWorse(Cards(reducedCards)) then return Some(Cards(reducedCards))
+        this.groupBySameValue
+          .map(c => if board.isWorse(c) then return Some(c))
+
+        this.groupBySameValue
+          .map(c => if c.size > board.size then
+              var reducedCards = Set.empty[CardInterface]
+
+              c.returnSet.toArray.slice(0, board.size).map { x => reducedCards = reducedCards + x }
+              if board.isWorse(Cards(reducedCards)) then return Some(Cards(reducedCards)))
         return None
 
     def bestCards : CardsInterface = 
