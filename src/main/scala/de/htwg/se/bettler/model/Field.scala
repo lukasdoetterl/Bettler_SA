c4a952c3415e0f98837448397d7f0b93bfff52f4
nickhoess <nick@futuresoftware.de>
2023-03-28 17:23:03 +0200

    changed foreach to map

--- src/main/scala/de/htwg/se/bettler/model/Field.scala
+++ src/main/scala/de/htwg/se/bettler/model/Field.scala
@@ -9,28 +9,28 @@ import scala.language.postfixOps
 
 case class Field(game : Game):
 
-  def eol = sys.props("line.separator")  
+  def eol = sys.props("line.separator")
 
-  def printCard(card : CardInterface) : String =
-    var s = card.toString()
-    s = s.filter(!"()".contains(_))
-    val r = "[" + s + "]"
-    return r
+  def printCard(card: CardInterface): String =
+    val s = card.toString().filter(!"()".contains(_))
+    return "[" + s + "]"
 
 
-  def printField() : String =
+  def printField(): String =
     var r = ""
     var i = 0
     game.getPlayers()
       .map(players =>
         i += 1
-          r += eol +bar() + "Player " + i + eol
-          players
-        .returnSet.foreach {
-        r += printCard(_)
-      })
+        r += eol + bar() + "Player " + i + eol
+        players
+          .returnSet.map {
+          r += printCard(_)
+        })
     r += eol + bar()
-    game.getBoard().returnSet.foreach{r+= printCard(_)}
+    game.getBoard().returnSet.map {
+      r += printCard(_)
+    }
     r += eol + bar()
     return r
 
