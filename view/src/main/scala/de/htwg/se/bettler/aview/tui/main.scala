42b03b84f1cfac413c4409fd196b34e8b1d5698e
nickhoess <nick@futuresoftware.de>
2023-05-09 16:02:37 +0200

    updated

--- view/src/main/scala/de/htwg/se/bettler/aview/tui/main.scala
+++ view/src/main/scala/de/htwg/se/bettler/aview/tui/main.scala
@@ -1,7 +1,11 @@
 package de.htwg.se.bettler.aview.tui
 
+import scala.Console.{BLUE, RESET}
+import scala.io.StdIn.readLine
+
 object main {
   @main def run =
+    println("Welcome to Bettler")
     println("\n" * 50)
     val tui = TUIRest()
       tui.run
